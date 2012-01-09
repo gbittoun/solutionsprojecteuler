927fa0e4a92b7517d5ed4430743ce90b41264c3b
guite <guite@guite-desktop.(none)>
2012-01-09 20:14:15 +0100

    Solution display at the end

--- main.cpp
+++ main.cpp
@@ -31,40 +31,12 @@
 #include "problems/problem_35.hpp"
 #include "problems/problem_36.hpp"
 #include "problems/problem_37.hpp"
+#include "problems/problem_38.hpp"
 
 using namespace std;
 using namespace Computing;
 
-#include <sstream>
-
 int main()
 {
-    for (long long idx = 1 ; idx < 10000 ; ++idx)
-    {
-        ostringstream stream;
-        long long factor = 1;
-        set<int> digits;
-
-        while(true)
-        {
-            stream << idx * factor;
-
-            for(size_t s = 0 ; s < stream.str().size() ; ++s)
-            {
-                digits.insert(stream.str().c_str()[s]);
-            }
-
-            if(stream.str().size() != digits.size())
-                break;
-            else if( (digits.find('0') == digits.end()) && (stream.str().size() == digits.size()) && (digits.size() == 9) )
-            {
-                cout << idx << " " << stream.str() << endl;
-                break;
-            }
-
-            ++factor;
-        }
-    }
-
-    return 0;
+    return problem_38();
 }
---
+++ problems/problem_38.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <set>
+#include <sstream>
+
+using namespace std;
+
+int problem_38()
+{
+    set<string> results;
+
+    for (long long idx = 1 ; idx < 10000 ; ++idx)
+    {
+        ostringstream stream;
+        long long factor = 1;
+        set<int> digits;
+
+        while(true)
+        {
+            stream << idx * factor;
+
+            for(size_t s = 0 ; s < stream.str().size() ; ++s)
+            {
+                digits.insert(stream.str().c_str()[s]);
+            }
+
+            if(stream.str().size() != digits.size())
+                break;
+            else if( (digits.find('0') == digits.end()) && (stream.str().size() == digits.size()) && (digits.size() == 9) )
+            {
+                cout << idx << " " << stream.str() << endl;
+                results.insert(stream.str());
+                break;
+            }
+
+            ++factor;
+        }
+    }
+
+    cout << endl << *results.rbegin() << endl;
+
+    return 0;
+}
