e8a39dae3518124b06704ce59613fe3f006768ce
guite <guite@guite-desktop.(none)>
2012-01-10 21:56:56 +0100

    Adding possible solution to problem 41

--- main.cpp
+++ main.cpp
@@ -37,7 +37,44 @@
 using namespace std;
 using namespace Computing;
 
+#include <sstream>
+
 int main()
 {
-    return problem_40();
+    set<long long> primes;
+    FillPrimes(primes, 10000000);
+
+    set<long long>::iterator it = primes.begin();
+
+    while(it != primes.end())
+    {
+        ostringstream stream;
+        stream << *it;
+        set<int> digits;
+
+        bool useless = false;
+
+        for(size_t idx = 0 ; idx < stream.str().size() ; ++idx)
+        {
+            if(digits.find(stream.str().c_str()[idx]) != digits.end())
+            {
+                useless = true;
+                break;
+            }
+            digits.insert(stream.str().c_str()[idx]);
+        }
+
+        if(useless)
+        {
+            ++it;
+            continue;
+        }
+
+        if(*digits.begin() == '1' && digits.size() == (*digits.rbegin() - *digits.begin() + 1) )
+            cout << *it << endl;
+
+        ++it;
+    }
+
+    return 0;
 }
