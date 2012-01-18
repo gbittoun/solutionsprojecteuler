40adc0c35a20767eac5205ef69ab9cbd592a005e
guite <guite@guite-desktop.(none)>
2012-01-18 21:52:42 +0100

    Beginning problem 54

--- main.cpp
+++ main.cpp
@@ -49,7 +49,86 @@
 using namespace std;
 using namespace Computing;
 
+#include <vector>
+
+class Card
+{
+    public:
+
+    // There is no meaning to Suit value, it's just useful to order them in a set
+    enum Suit {Spade = 0, Heart = 1, Diamond = 2, Club = 3};
+    enum Value {Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13, Ace = 14};
+
+    private:
+
+    Suit s;
+    Value v;
+
+    public:
+
+    Card(Suit _s, Value _v) :
+        s(_s),
+        v(_v)
+    {
+    }
+
+    Suit GetSuit() const
+    {
+        return s;
+    }
+
+    Value GetValue() const
+    {
+        return v;
+    }
+};
+
+struct CardComparer
+{
+    bool operator()(const Card & c0, const Card & c1)
+    {
+        if(c0.GetValue() < c1.GetValue())
+            return true;
+        else if(c0.GetValue() > c1.GetValue())
+            return false;
+        else if(c0.GetSuit() < c1.GetSuit())
+            return true;
+        else if(c0.GetSuit() > c1.GetSuit())
+            return false;
+
+        return false;
+    }
+};
+
+class Hand
+{
+    set<Card, CardComparer> cards;
+
+    map<Card::Suit, vector<Card::Value> > cardsBySuit;
+    map<Card::Value, vector<Card::Suit> > cardsByValue;
+
+    public:
+
+    enum Figure {HighCard = 0, Pair = 1, TwoPair = 2, ThreeOfAKind = 3, Straight = 4, Flush = 5, FullHouse = 6, FourOfAKind = 7, StraightFlush = 8};
+
+    void PushCard(Card c)
+    {
+        cards.insert(c);
+        cardsBySuit[c.GetSuit()].push_back(c.GetValue());
+        cardsByValue[c.GetValue()].push_back(c.GetSuit());
+    }
+
+    Figure GetFigure()
+    {
+        for(map<Card::Value, vector<Card::Suit> >::iterator it = cardsByValue.begin() ; it != cardsByValue.end() ; ++it)
+        {
+        }
+
+        return Pair;
+    }
+};
+
 int main()
 {
-    return problem_51();
+    return 0;
 }
