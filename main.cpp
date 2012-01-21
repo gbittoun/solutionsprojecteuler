3d15ef1702a03930648bfdc4703a235176742a48
guite <guite@guite-desktop.(none)>
2012-01-21 22:29:22 +0100

    problem 55 refactoring

--- main.cpp
+++ main.cpp
@@ -46,109 +46,13 @@
 #include "problems/problem_51.hpp"
 #include "problems/problem_52.hpp"
 #include "problems/problem_54.hpp"
+#include "problems/problem_55.hpp"
 #include "problems/problem_56.hpp"
 
 using namespace std;
 using namespace Computing;
 
-#include <vector>
-
-namespace Computing
-{
-    template<int N>
-    FatNumber<N> MakePalindrome(const FatNumber<N> & x)
-    {
-        int limit = (sizeof(x.v) / sizeof(int)) - 1;
-        int idx = 0;
-
-        while(limit > 0 && x.v[limit] == 0)
-            --limit;
-
-        vector<char> digits;
-
-        while(idx <= limit)
-        {
-            int tmp = x.v[idx++];
-            if(idx == (limit+1))
-            {
-                while(tmp > 0)
-                {
-                    digits.push_back(tmp % 10);
-                    tmp /= 10;
-                }
-            }
-            else
-            {
-                for(int count = 0 ; count < 4 ; ++count)
-                {
-                    digits.push_back(tmp % 10);
-                    tmp /= 10;
-                }
-            }
-        }
-
-        idx = 0;
-        int buf = 0, count = 0, factor = 1;
-        FatNumber<N> ret;
-
-        for(vector<char>::reverse_iterator it = digits.rbegin() ; it != digits.rend() ; ++it)
-        {
-            buf += *it * factor;
-            ++count;
-            factor *= 10;
-
-            if(count == 4)
-            {
-                ret.v[idx++] = buf;
-                buf /= 10000;
-                count = 0;
-                factor = 1;
-            }
-        }
-        ret.v[idx++] = buf;
-
-        return ret;
-    }
-
-    template<int N>
-    bool IsPalindrome(const FatNumber<N> & x)
-    {
-        return (x == MakePalindrome<N>(x));
-    }
-}
-
 int main()
 {
-    long long sum = 0;
-
-    for(int idx = 1 ; idx < 10000 ; ++idx)
-    {
-        FatNumber<1024> a = idx;
-        FatNumber<1024> b = MakePalindrome(a);
-
-        bool p = true;
-        for(int jdx = 0 ; jdx < 50 ; ++jdx)
-        {
-            cout << a << " + " << b << " = ";
-            FatNumber<1024> c = (a + b);
-            cout << c << endl;
-
-            a = c;
-            b = MakePalindrome(a);
-
-
-            if(IsPalindrome(c))
-            {
-                p = false;
-                break;
-            }
-        }
-
-        if(p)
-            ++sum;
-    }
-
-    cout << sum << endl;
-
-    return 0;
+    return problem_55();
 }
---
+++ problems/problem_55.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+
+using namespace std;
+
+#include "problem_20.hpp"
+#include "problem_55.hpp"
+
+using namespace Computing;
+
+int problem_55()
+{
+    long long sum = 0;
+
+    for(int idx = 1 ; idx < 10000 ; ++idx)
+    {
+        FatNumber<1024> a = idx;
+        FatNumber<1024> b = MakePalindrome(a);
+
+        bool p = true;
+        for(int jdx = 0 ; jdx < 50 ; ++jdx)
+        {
+            cout << a << " + " << b << " = ";
+            FatNumber<1024> c = (a + b);
+            cout << c << endl;
+
+            a = c;
+            b = MakePalindrome(a);
+
+
+            if(IsPalindrome(c))
+            {
+                p = false;
+                break;
+            }
+        }
+
+        if(p)
+            ++sum;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
