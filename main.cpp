e6dab1e37c9282b1894366b35a3ff437a2b2c7ca
guite <guite@guite-desktop.(none)>
2012-02-08 23:08:00 +0100

    problem 66 solved

--- main.cpp
+++ main.cpp
@@ -1,35 +1,6 @@
-#include "Computing/IntegerOperations.hpp"
-
-#include <math.h>
-
-using namespace std;
-using namespace Computing;
+#include "problems/problem_66.hpp"
 
 int main()
 {
-    FatNumber<64> maxx = 0;
-    long long finalD = 0;
-
-    for(long long D = 2 ; D <= 1000 ; ++D)
-    {
-        if(floor(sqrt(D)) == ceil(sqrt(D)))
-            continue;
-
-        vector<long long> c;
-
-        GetSqrtCoeff(D, c);
-        FatFraction<64> fract = DevelopSqrtCoeff<64>(c, 1, true);
-
-        cout << D << " " << fract.u << " / " << fract.d << " => " << fract.u * fract.u - fract.d * fract.d * D << endl;
-
-        if(maxx < fract.u && ((fract.u * fract.u) - (fract.d * fract.d * D)) == -1 )
-        {
-            finalD = D;
-            maxx = fract.u;
-        }
-    }
-
-    cout << endl << finalD << " " << maxx << endl;
-
-    return 0;
+    return problem_66();
 }
---
+++ problems/problem_66.cpp
@@ -0,0 +1,42 @@
+#include "problem_66.hpp"
+#include "../Computing/IntegerOperations.hpp"
+
+#include <math.h>
+
+using namespace std;
+using namespace Computing;
+
+int problem_66()
+{
+    FatNumber<64> maxxplus = 0, maxxminus = 0;
+    long long finalDplus = 0, finalDminus = 0;
+
+    for(long long D = 2 ; D <= 1000 ; ++D)
+    {
+        if(floor(sqrt(D)) == ceil(sqrt(D)))
+            continue;
+
+        vector<long long> c;
+
+        GetSqrtCoeff(D, c);
+        FatFraction<64> fract = DevelopSqrtCoeff<64>(c, 1, true);
+
+        cout << D << " " << fract.u << " / " << fract.d << " => " << fract.u * fract.u - fract.d * fract.d * D << endl;
+
+        if(maxxminus < fract.u && ((fract.u * fract.u) - (fract.d * fract.d * D)) == -1 )
+        {
+            finalDminus = D;
+            maxxminus = fract.u;
+        }
+        else if (maxxplus < fract.u && ((fract.u * fract.u) - (fract.d * fract.d * D)) == 1 )
+        {
+            finalDplus = D;
+            maxxplus = fract.u;
+        }
+    }
+
+    cout << endl << finalDminus << " " << maxxminus;
+    cout << endl << finalDplus << " " << maxxplus << endl;
+
+    return 0;
+}
